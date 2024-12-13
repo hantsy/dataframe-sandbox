b7b1cf8fd65408a62b3ff97bd0efa5b06f545e2b
hantsy <hantsy@gmail.com>
2024-12-13 10:14:10 +0800

    chore: clean codes

--- src/main/kotlin/com/example/CsvExample.kt
+++ src/main/kotlin/com/example/CsvExample.kt
@@ -3,18 +3,24 @@ package com.example
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.annotations.ColumnName
 import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
+import org.jetbrains.kotlinx.dataframe.api.ParserOptions
 import org.jetbrains.kotlinx.dataframe.api.convertTo
-import org.jetbrains.kotlinx.dataframe.api.print
 import org.jetbrains.kotlinx.dataframe.api.parser
+import org.jetbrains.kotlinx.dataframe.api.print
 import org.jetbrains.kotlinx.dataframe.api.schema
 import org.jetbrains.kotlinx.dataframe.api.toList
 import org.jetbrains.kotlinx.dataframe.io.readCsv
 import java.math.BigDecimal
+import kotlin.reflect.typeOf
 
 fun main() {
     println("Hello World!")
     val dataFrame = DataFrame
-        .readCsv(CsvExampleDataModel::class.java.getResourceAsStream("/sample.csv")!!)
+        .readCsv(
+            inputStream = CsvExampleDataModel::class.java.getResourceAsStream("/sample.csv")!!,
+            // https://github.com/Kotlin/dataframe/issues/998
+            parserOptions = ParserOptions(skipTypes = setOf(typeOf<Char>()))
+        )
     dataFrame.schema().print()
     val result = dataFrame
         .convertTo<CsvExampleDataModel> {
