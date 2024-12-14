a15599e7b2d2044188151e615ba8da8b4c8f53f1
hantsy <hantsy@gmail.com>
2024-12-14 09:45:13 +0800

    chore: clean codes

--- src/main/kotlin/com/example/CsvExample.kt
+++ src/main/kotlin/com/example/CsvExample.kt
@@ -12,6 +12,7 @@ import org.jetbrains.kotlinx.dataframe.api.toDataFrame
 import org.jetbrains.kotlinx.dataframe.api.toList
 import org.jetbrains.kotlinx.dataframe.io.readCsv
 import org.jetbrains.kotlinx.dataframe.io.toCsv
+import org.jetbrains.kotlinx.dataframe.io.toCsvStr
 import java.math.BigDecimal
 import kotlin.reflect.typeOf
 
@@ -34,11 +35,14 @@ fun main() {
         .toList()
     println("converted result:$result")
 
-    val exportedCsv = listOf(
+    val exportedDf = listOf(
         CsvExampleDataModel(BigDecimal("10.2"), "1234", ACHType.SAME_DAY),
         CsvExampleDataModel(BigDecimal("11.20"), "4567", ACHType.NEXT_DAY)
-    ).toDataFrame().toCsv()
-    println("expected csv: $exportedCsv")
+    ).toDataFrame()
+    val exportedCsv = exportedDf.toCsv()
+    println("old expected csv(Apache Commons Csv): \n$exportedCsv")
+    val newExportedCsv = exportedDf.toCsvStr()
+    println("new expected csv: \n$newExportedCsv")
 }
 
 enum class ACHType(val symbol: String) {
